b17c66b45e911f52718afc2138c0e61383b5f8ae
Arthur <arthur.sw@gmail.com>
2016-12-28 14:46:08 +0100

    updates styles

--- index.php
+++ index.php
@@ -15,53 +15,57 @@
 
 								<!-- </header> -->
 
-								<section class="entry-content ">
-								<?php 
-									if ( has_post_thumbnail() ) {
-										the_post_thumbnail();
-									} else {
-										$first_image_thumb_url = get_first_image_thumb_url(get_the_ID());
+								<div class="am-preview-frame">
 
-										if($first_image_thumb_url == '') {
-											$first_image_thumb_url = get_first_image_url_from_content(get_the_content());
+									<section class="entry-content ">
+									<?php 
+										if ( has_post_thumbnail() ) {
+											the_post_thumbnail();
+										} else {
+											$first_image_thumb_url = get_first_image_thumb_url(get_the_ID());
+
+											if($first_image_thumb_url == '') {
+												$first_image_thumb_url = get_first_image_url_from_content(get_the_content());
+											}
+
+											printf('<a href="' . get_the_permalink() . '" rel="bookmark" >');
+
+											if(!empty($first_image_thumb_url)) {
+												printf( '<img src="' . $first_image_thumb_url . '" alt="' . single_post_title() . '-thumb" />');
+												
+											}
+											else
+											{
+												the_excerpt();
+											}
+											printf('</a>');
 										}
+									?>
+									</section>
 
-										printf('<a href="' . get_the_permalink() . '" rel="bookmark" >');
-
-										if(!empty($first_image_thumb_url)) {
-											printf( '<img src="' . $first_image_thumb_url . '" alt="' . single_post_title() . '-thumb" />');
-											
-										}
-										else
-										{
-											the_excerpt();
-										}
-										printf('</a>');
-									}
-								?>
-								</section>
-
-								<footer class="article-footer ">
-                 				<?php // printf( '<p class="footer-category">%1$s</p>' , get_the_category_list(' ') ); ?>
-                  				<?php // the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
+									<footer class="article-footer ">
+	                 				<?php // printf( '<p class="footer-category">%1$s</p>' , get_the_category_list(' ') ); ?>
+	                  				<?php // the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 
 
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline entry-meta vcard">
-												<?php printf( '%1$s',
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
-                       								/* the author of the post */
-                       								/* 
+										<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+										<p class="byline entry-meta vcard">
+													<?php printf( '%1$s',
+	                       								/* the time the post was published */
+	                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
+	                       								/* the author of the post */
+	                       								/* 
 
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope 
-                       								itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>' 
-                       								
-                       								*/
-                    							); ?>
-									</p>
-									                  				
-								</footer>
+	                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope 
+	                       								itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>' 
+	                       								
+	                       								*/
+	                    							); ?>
+										</p>
+										                  				
+									</footer>
+
+								</div> <!-- / div.am-preview-frame -->
 
 							</article>
 
